c968f95c591993d05d19b460363cdd7a1c0871a6
cqing <SiqingIII@163.com>
2018-10-19 14:49:16 +0800

    精简微信开发文件，删除不必要文件

--- test/setTimeOut.js
+++
@@ -1,5 +0,0 @@
-setTimeout(a,2000)
-
-function a (){
-    console.log('2miao')
-}
--- test/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$openId = isset($_POST['openId']) ? htmlspecialchars($_POST['openId']) : '';
-// $url = isset($_POST['url']) ? htmlspecialchars($_POST['url']) : '';
-echo '网站名: ' . $openId;
-echo "\n";
-
-?>
